d162c96835f8def171e6d6524b3621790b31cdd6
Dilmurod <shukurovd@gmail.com>
2024-07-24 11:13:57 +0000

    bir kunda bir application

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -6,12 +6,17 @@ use App\Jobs\SendEmailJob;
 use App\Mail\ApplicationCreated;
 use App\Models\Application;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class ApplicationController extends Controller
 {
     public function store(Request $request){
+
+        if($this->checkDate()){
+            return redirect()->back()->with('error', 'You can create only 1 application a day');
+        }
        
         if($request->hasFile('file')){
             $name = $request->file('file')->getClientOriginalName();
@@ -40,5 +45,21 @@ class ApplicationController extends Controller
         
         
         return redirect()->back();
+
     }
+
+    protected function checkDate(){
+
+        
+        $last_application = auth()->user()->applications()->latest()->first();
+        if($last_application ==null){
+            return false;
+        }
+        $last_app_date = Carbon::parse($last_application->created_at)->format("Y-m-d");
+        $today =Carbon::now()->format("Y-m-d");
+
+        if($last_app_date == $today){
+            return true;
+        }
+    }   
 }
